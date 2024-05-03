39234471717fef5e4287dc6f1899bbaed58c94c8
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-05-03 19:51:06 -0300

    Projeto Concluido

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿int Numero;
+
+Console.WriteLine("Digite um numero...: ");
+Numero = Convert.ToInt32(Console.ReadLine());
+
+if (Numero < 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Esse Numero é Negativo");
+    Console.ResetColor();
+}
+else
+{
+    if (Numero > 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Esse numero é Positivo");
+        Console.ResetColor();
+    }
+    else
+    {
+        Console.ForegroundColor = ConsoleColor.DarkMagenta;
+        Console.WriteLine("Esse é Numero Zero");
+        Console.ResetColor();
+
+    }
+}
